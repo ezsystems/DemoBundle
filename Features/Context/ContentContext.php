d8726059f1ec6b56b759601352974c66c052643e
MLoureiro <mloureiro.dev@gmail.com>
2014-05-02 10:50:45 +0100

    BDD: add tag cloud feature implementation

--- Features/Context/ContentContext.php
+++ Features/Context/ContentContext.php
@@ -14,6 +14,8 @@ namespace EzSystems\DemoBundle\Features\Context;
 
 use EzSystems\DemoBundle\Features\Context\FeatureContext;
 use Behat\Behat\Context\Step;
+use Behat\Behat\Exception\PendingException;
+use PHPUnit_Framework_Assert as Assertion;
 
 /**
  * Feature context.
@@ -26,6 +28,7 @@ class ContentContext extends FeatureContext
         $this->pageIdentifierMap += array(
             "search" => "/content/search",
             "site map" => "/content/view/sitemap/2",
+            "tag cloud" => "/content/view/tagcloud/2",
         );
 
         // specify the tags for specific content
@@ -36,21 +39,67 @@ class ContentContext extends FeatureContext
     }
 
     /**
-     * @When /^I check site map for Location "([^"]*)"$/
+     * This is an helper to retrieve the id of a Content object so that it can
+     * be used on links for locations
      *
-     * @todo Change the way it fetches the ID for the specific page
+     * @param string $text Text identifier for a location
+     *
+     * @return int
+     *
+     * @throws PendingException When location is not defined
+     */
+    protected function getDemoObjectLocationByText( $text )
+    {
+        switch ( strtolower( $text ) )
+        {
+            case 'shopping':
+                return 74;
+            case 'getting started':
+                return 61;
+        }
+
+        throw new PendingException( "Location for '$text' not defined." );
+    }
+
+    /**
+     * @When /^I check site map for Location "(?P<location>[^"]*)"$/
      */
     public function iCheckSiteMapForLocation( $location )
     {
-        switch ( strtolower( $location ) ) {
-            case "shopping":
-                $id = 74;
-                break;
-
-            default:
-                throw new PendingException( "Location for '$location' not defined." );
-        }
-
+        $id = $this->getDemoObjectLocationByText( $location );
         return new Step\When( 'I am on "/content/view/sitemap/' . $id . '"' );
+    }
+
+    /**
+     * @Given /^I am on tag page for "(?P<tag>[^"]*)"$/
+     */
+    public function iAmOnTagPageFor( $tag )
+    {
+        return new Step\When( "I am on \"/content/keyword/$tag\"" );
+    }
+
+    /**
+     * @When /^I go to tag cloud for "(?P<tag>[^"]*)"$/
+     */
+    public function iCheckTagCloudFor( $tag )
+    {
+        $id = $this->getDemoObjectLocationByText( $tag );
+        return new Step\When( "I am on \"/content/view/tagcloud/$id\"" );
+    }
+
+    /**
+     * @Then /^I see tag page for "(?P<tag>[^"]*)"$/
+     */
+    public function iSeeTagPageFor( $tag )
+    {
+        $currentUrl = $this->getUrlWithoutQueryString( $this->getSession()->getCurrentUrl() );
+
+        $expectedUrl = $this->locatePath( "/content/keyword/$tag" );
+
+        Assertion::assertEquals(
+            $expectedUrl,
+            $currentUrl,
+            "Unexpected URL of the current site. Expected: '$expectedUrl'. Actual: '$currentUrl'."
+        );
     }
 }
