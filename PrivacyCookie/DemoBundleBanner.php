b89a4afac76e6d7ab015a753d78ae1c581150052
Rafał Toborek <rafal.toborek@ez.no>
2015-06-22 12:09:13 +0200

    Added additional checks if privacy policy Content object exists (in case of loading demo_clean data)

--- PrivacyCookie/DemoBundleBanner.php
+++ PrivacyCookie/DemoBundleBanner.php
@@ -6,8 +6,8 @@
 namespace EzSystems\DemoBundle\PrivacyCookie;
 
 use eZ\Publish\API\Repository\LocationService;
+use eZ\Publish\API\Repository\ContentService;
 use eZ\Publish\API\Repository\URLAliasService;
-use eZ\Publish\API\Repository\Values\Content\Content;
 use EzSystems\PrivacyCookieBundle\Banner\Banner;
 use EzSystems\DemoBundle\PrivacyCookie;
 use Symfony\Component\Translation\Translator;
@@ -17,17 +17,57 @@ use Symfony\Component\Translation\Translator;
  */
 class DemoBundleBanner implements DemoBundleBannerFactory
 {
-    public static function build( Content $bannerContent, LocationService $locationService, URLAliasService $urlAliasService, Translator $translator )
+    public static function build( array $bannerOptions, LocationService $locationService, ContentService $contentService, URLAliasService $urlAliasService, Translator $translator )
     {
+        $contentId = null;
+
+        // check if `Content` object path exists and if so, get associated `contentId`
+        if ( !empty( $bannerOptions[ 'pathString' ] ) )
+        {
+            try
+            {
+                $alias = $urlAliasService->lookup( $bannerOptions['pathString'] );
+                $contentId = $locationService->loadLocation( $alias->destination )->contentId;
+            }
+            catch ( \Exception $e )
+            {
+            }
+        }
+
+        $content = null;
+
+        // check if `contentId` exists and if so, get associated `Content` object
+        if ( $contentId )
+        {
+            try
+            {
+                $content = $contentService->loadContent( $contentId );
+            }
+            catch ( \Exception $e )
+            {
+            }
+        }
+
         $banner = new Banner();
 
-        $banner->caption = $bannerContent->getFieldValue( 'intro' )->xml->textContent;
-        $banner->policyPageUrl = $urlAliasService->reverseLookup(
-            $locationService->loadLocation( $bannerContent->contentInfo->mainLocationId )
-        )->path;
-        $banner->learnMoreText = $translator->trans( 'Learn more' );
-        $banner->cookieName = "eZDemoPrivacyCookie";
-        $banner->cookieValidity = 365;
+        if ( $content )
+        {
+            // fill `Banner` object using `Content` data
+            $banner->caption = $content->getFieldValue( 'intro' )->xml->textContent;
+            $banner->policyPageUrl = $urlAliasService->reverseLookup(
+                $locationService->loadLocation( $content->contentInfo->mainLocationId )
+            )->path;
+        }
+        else
+        {
+            // fill `Banner` object using defaults
+            $banner->caption = $bannerOptions[ 'bannerCaption' ];
+            $banner->policyPageUrl = $bannerOptions[ 'learnMoreUrl' ];
+        }
+
+        $banner->learnMoreText = $translator->trans( $bannerOptions[ 'learnMoreText' ] );
+        $banner->cookieName = $bannerOptions[ 'cookieName' ];
+        $banner->cookieValidity = $bannerOptions[ 'validityDays' ];
 
         return $banner;
     }
--- PrivacyCookie/DemoBundleBannerFactory.php
+++ PrivacyCookie/DemoBundleBannerFactory.php
@@ -7,23 +7,20 @@ namespace EzSystems\DemoBundle\PrivacyCookie;
 
 use eZ\Publish\API\Repository\LocationService;
 use eZ\Publish\API\Repository\URLAliasService;
-use eZ\Publish\API\Repository\Values\Content\Content;
-use eZ\Publish\API\Repository\Values\Content\Location;
-use eZ\Publish\API\Repository\Values\Content\URLAlias;
-use EzSystems\PrivacyCookieBundle\Banner\Banner;
-use EzSystems\PrivacyCookieBundle\Banner\BannerFactory;
+use eZ\Publish\API\Repository\ContentService;
 use Symfony\Component\Translation\Translator;
 
 interface DemoBundleBannerFactory
 {
     /**
-     * Builds the privacy cookie banner object, using any kind of data source.
+     * Builds the privacy cookie `Banner` object, using any kind of data source.
      *
-     * @param $bannerContent \eZ\Publish\API\Repository\Values\Content\Content
+     * @param $bannerOptions array
      * @param $locationService \eZ\Publish\API\Repository\LocationService
+     * @param $contentService \eZ\Publish\API\Repository\ContentService
      * @param $urlAliasService \eZ\Publish\API\Repository\URLAliasService
      * @param $translator \Symfony\Component\Translation\Translator
      * @return \EzSystems\PrivacyCookieBundle\Banner\Banner
      */
-    public static function build(Content $bannerContent, LocationService $locationService, URLAliasService $urlAliasService, Translator $translator);
+    public static function build( array $bannerOptions, LocationService $locationService, ContentService $contentService, URLAliasService $urlAliasService, Translator $translator );
 }
