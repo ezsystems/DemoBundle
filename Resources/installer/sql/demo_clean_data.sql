de05b0a7c841ca5b6a6d08a7e6c15db393b5cf8e
Bertrand Dunogier <bd@ez.no>
2015-07-17 11:25:21 +0200

    Fixed demodata

--- Resources/installer/sql/demo_clean_data.sql
+++ Resources/installer/sql/demo_clean_data.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 5.5.43, for debian-linux-gnu (x86_64)
 --
--- Host: localhost    Database: democlean
+-- Host: localhost    Database: temp
 -- ------------------------------------------------------
 -- Server version	5.5.43-0ubuntu0.14.04.1
 
@@ -3385,4 +3385,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-07-16 17:30:29
+-- Dump completed on 2015-07-17 11:27:00
